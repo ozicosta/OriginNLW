b65c8272579258d3a30db09aac3773c111fedbdd
Ozieni Costa <ozieni.costa@gmail.com>
2021-10-24 11:49:18 -0300

    mod

---
+++ myscript.js
@@ -0,0 +1,39 @@
+ //camelCase / PascalCase / snake_case --> boa práticas p nomes de variáveis
+
+   const userSocialMedia = { //Cria de objeto
+      //  github:'ozicosta',
+       instagram: 'ozieni_costa'
+     }
+
+   function mudarLinkRedeSocial() {
+      // document.getElementById('userName'). // -> uma das formas para alterar valor no html usando Id
+      // textContent='Ozi Costa'
+
+      //userName.textContent = 'Ozii Costa' //forma simplificada para alterar valor dentro do html
+
+      for(let li of socialLinks.children){
+     // alert(li.getAttribute('class')) ou -->
+      const social = li.getAttribute('class')
+     // alert(social)
+
+   li.children[0].href = `https://www.${social}.com/${userSocialMedia[social]}`
+
+      // alert(li.children[0].href)
+    }
+}
+          mudarLinkRedeSocial()
+           
+   function userGithubProfile(){
+      const url =` https://api.github.com/users/${userSocialMedia.github}`
+
+      fetch(url)
+      .then(resposta => resposta.json())
+      .then(data => {
+        // userName.textContent = data.name
+        // userBio.textContent = data.bio
+        // user
+      })  
+   }
+
+    userGithubProfile()
+        
